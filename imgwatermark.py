5d0533b9fd8088829d02a4a01fa8d3dd15f602ea
Ernesto Alejandro Nappi <eanappi@gmail.com>
2021-05-10 16:34:47 -0300

    FIX - Add path for watermark image

--- imgwatermark.py
+++ imgwatermark.py
@@ -1,16 +1,29 @@
 import os
+import glob
 import argparse
+from PIL import Image
 
-arguments = argparse.ArgumentParser(
-    description="Add watermarks in various positions and formats."
-)
-arguments.add_argument("-s", "--source",
-                       help="Folder source", required=True)
-arguments.add_argument("-t", "--target",
-                       help="Folder target", required=True)
-arguments.add_argument("-p", "--position",
-                       help="Position of watermark", choices=[
-                           "all", "upper_left_corner", "upper_right_corner", "lower_left_corner", "lower_right_corner"], default="lower_right_corner")
-args = arguments.parse_args()
+# Configure arguments of global use
 
-print(args)
+
+def get_arguments():
+    arguments = argparse.ArgumentParser(
+        description="Add watermarks in various positions and formats."
+    )
+    arguments.add_argument("-s", "--source",
+                           help="Path of folder source", required=True)
+    arguments.add_argument("-t", "--target",
+                           help="Path of folder target", required=True)
+    arguments.add_argument("-w", "--watermark",
+                           help="Path of watermark image", required=True)
+    arguments.add_argument("-p", "--position",
+                           help="Position of watermark", choices=[
+                               "all", "upper_left_corner", "upper_right_corner", "lower_left_corner", "lower_right_corner"], default="lower_right_corner")
+    return arguments.parse_args()
+
+def manipuling_markwaterimage():
+    pass
+
+def merge_images(folder_src, folder_tar):
+    pass
+
